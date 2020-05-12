849985615cdda85a56668c164c72fdc8cc3dfd4d
paul_ <paul_@192.168.1.97>
2020-05-11 21:19:28 -0500

    Inicializacion del proyecto.

---
+++ src/ec/edu/ups/modelo/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+/**
+ *
+ * @author paul_
+ */
+public class Usuario {
+    
+    public String cedula;
+    public String nombre;
+    public String apellido;
+    public String correo;
+    public String contrasenia;
+    
+}
